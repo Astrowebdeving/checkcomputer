9932758ada0601688dff6d5c3dfada64f23e7f89
worstwebdev <94124964+worstwebdev@users.noreply.github.com>
2024-06-28 15:09:55 -0500

    Add files via upload

--- comm.py
+++ comm.py
@@ -23,21 +23,13 @@ os.system(f"./secondshelltest.sh")
 os.system(f"echo Outputting memory usage processes")
 os.system(f"./memoryshell.sh")
 #subprocess.run(["./", "newshelltest.sh"])
-
-print('My rank is', worker, ". The size is", size, ". The IP is", ip)
+os.system("./allinfind.sh")
+f = open("allinfile.txt", "r")
+contents = f.read()
+print('My rank is', worker, ". The size is", size, ". The IP is", ip, ". ", contents)
+f.close()
  
-os.system("echo Total CPU Usage is:")
-#os.system("mpstat")
-#os.system("""top -bn2 | grep "Cpu(s)" | \
-#           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-#           awk '{print 100 - $1"%"}'
-#""")
-os.system("""top -bn2 | grep "Cpu(s)" | awk '{print $2+$4}' | cat | awk 'NR>1 {print $1}'""")
-os.system("echo Total RAM usage is:")
 
-os.system("free -h | awk '{print $3}' | tr -d 'fre, ' | sed 's/ //g' | awk 'NR<3 {print $1}'")
-os.system("echo Total Disk Space Usage is:")
-os.system("df -m | awk '{sum+=$5;} END{print sum;}'")
 
 
 
