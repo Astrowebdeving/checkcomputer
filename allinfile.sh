88daa8e97be28b107a4b326dd4e30c1b9eb52c3c
worstwebdev <94124964+worstwebdev@users.noreply.github.com>
2024-06-28 15:10:11 -0500

    Create allinfile.sh

---
+++ allinfile.sh
@@ -0,0 +1,7 @@
+touch allinfile.txt
+
+totalcpu=$(top -bn2 | grep "Cpu(s)" | awk '{print $2+$4}' | cat | awk 'NR>1 {print $1}')
+totalram=$(free -h | awk '{print $3}' | tr -d 'fre, ' | sed 's/ //g' | awk 'NR<3 {print $1}')
+totaldisk=$(df -m | awk '{sum+=$5;} END{print sum;}')
+
+echo The cpu usage is $totalcpu, the total ram usage is $totalram, the total disk usage is $totaldisk. >> allinfile.txt
