0c5785e3e083251af2c1dd4cb733edf6f7316f26
worstwebdev <94124964+worstwebdev@users.noreply.github.com>
2024-06-28 15:28:00 -0500

    Update allinfile.sh

--- allinfile.sh
+++ allinfile.sh
@@ -5,6 +5,5 @@ touch allinfile_${macaddress}.txt
 totalcpu=$(top -bn2 | grep "Cpu(s)" | awk '{print $2+$4}' | cat | awk 'NR>1 {print $1}')
 totalram=$(free -h | awk '{print $3}' | tr -d 'fre, ' | sed 's/ //g' | awk 'NR<3 {print $1}')
 totaldisk=$(df -m | awk '{sum+=$5;} END{print sum;}')
-
 echo The cpu usage is $totalcpu, the total ram usage is $totalram, the total disk usage is $totaldisk. >> allinfile_${macaddress}.txt
 cat allinfile_${macaddress}.txt
