77177680b126e2f08ef0e5e33bbd26ddb29d78cc
worstwebdev <94124964+worstwebdev@users.noreply.github.com>
2024-07-19 09:38:40 -0500

    Update comm.py

--- comm.py
+++ comm.py
@@ -3,6 +3,8 @@ import socket
 from collections import Counter
 from time import sleep
 import os
+import time
+import subprocess
 
 comm = MPI.COMM_WORLD
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -23,13 +25,12 @@ os.system(f"./secondshelltest.sh")
 os.system(f"echo Outputting memory usage processes")
 os.system(f"./memoryshell.sh")
 #subprocess.run(["./", "newshelltest.sh"])
-os.system("./allinfind.sh")
-f = open("allinfile.txt", "r")
-contents = f.read()
-print('My rank is', worker, ". The size is", size, ". The IP is", ip, ". ", contents)
-f.close()
- 
-
-
-
-
+os.system("./allinfile.sh")
+#endtime = time.process_time()
+#os.system('date +"%T.%N"')
+result = subprocess.run(['date', '+"%T.%N"'], capture_output=True, text=True)
+completion_time = result.stdout.strip()
+print(completion_time)
+bash_command = f'export {'timeend'}="{completion_time}"'
+with open(os.getenv('HOME') + '/.bashrc', 'a') as bashrc:
+     bashrc.write(f'\n{bash_command}\n')
