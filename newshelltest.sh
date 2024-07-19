9eabd0223ecf7423b3b86ac64ebbfedc719b1792
worstwebdev <94124964+worstwebdev@users.noreply.github.com>
2024-07-19 09:40:08 -0500

    Update newshelltest.sh

--- newshelltest.sh
+++ newshelltest.sh
@@ -2,25 +2,26 @@
 touch storage.txt
 touch storage.csv
 macaddress=$(ip -o link show dev eno1 | grep -Po 'ether \K[^ ]*')
-touch "updatedstorage3_${macaddress}.csv"
+ip=$(hostname -I | awk '{print $1}')
+name=$(cat /etc/hosts | grep $ip | awk '{print $2}')
+touch "updatedstorage_${name}.csv"
 dateinfo=$(date)
 
 df -m | grep t | cat> storage.txt
 awk '{OFS=","};NR>1 {print $1,$2,$3,$4,$5,$NF}' storage.txt > storage.csv
-awk -v data="$dateinfo" -F"," 'BEGIN{OFS = ","}{$7=data; print}' storage.csv > "updatedstorage_${macaddress}.csv"
-touch "sysname_${macaddress}.txt"
-touch "overallstorage_${macaddress}.txt"
-touch "amountused_${macaddress}.txt"
-touch "amountavailable_${macaddress}.txt"
-touch "usage_${macaddress}.txt"
-touch "location_${macaddress}.txt"
-touch "date_${macaddress}.txt"
+awk -v data="$dateinfo" -F"," 'BEGIN{OFS = ","}{$7=data; print}' storage.csv > "updatedstorage_${name}.csv"
+touch "sysname_${name}.txt"
+touch "overallstorage_${name}.txt"
+touch "amountused_${name}.txt"
+touch "amountavailable_${name}.txt"
+touch "usage_${name}.txt"
+touch "location_${name}.txt"
+touch "date_${name}.txt"
 
-awk 'NR>1 {print $1}' storage.txt > "sysname_${macaddress}.txt"
-awk 'NR>1 {print $2}' storage.txt > "overallstorage_${macaddress}.txt"
-awk 'NR>1 {print $3}' storage.txt > "amountused_${macaddress}.txt"
-awk 'NR>1 {print $4}' storage.txt > "amountavailable_${macaddress}.txt"
-awk 'NR>1 {print $5}' storage.txt > "usage_${macaddress}.txt"
-awk 'NR>1 {print $6}' storage.txt > "location_${macaddress}.txt"
-awk -F"," 'NR>1 {print $7}' "updatedstorage_${macaddress}.csv" > "date_${macaddress}.txt" 
-
+awk 'NR>1 {print $1}' storage.txt > "sysname_${name}.txt"
+awk 'NR>1 {print $2}' storage.txt > "overallstorage_${name}.txt"
+awk 'NR>1 {print $3}' storage.txt > "amountused_${name}.txt"
+awk 'NR>1 {print $4}' storage.txt > "amountavailable_${name}.txt"
+awk 'NR>1 {print $5}' storage.txt > "usage_${name}.txt"
+awk 'NR>1 {print $6}' storage.txt > "location_${name}.txt"
+awk -F"," 'NR>1 {print $7}' "updatedstorage_${name}.csv" > "date_${name}.txt" 
