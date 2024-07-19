ec9780f6544a8c08881086ff08bb9b888c05336b
worstwebdev <94124964+worstwebdev@users.noreply.github.com>
2024-07-19 09:39:54 -0500

    Update memoryshell.sh

--- memoryshell.sh
+++ memoryshell.sh
@@ -2,9 +2,11 @@
 touch memoryram.txt
 touch memoryram.csv
 macaddress=$(ip -o link show dev eno1 | grep -Po 'ether \K[^ ]*')
+ip=$(hostname -I | awk '{print $1}')
+name=$(cat /etc/hosts | grep $ip | awk '{print $2}')
 dateinfo=$(date)
-touch "updatedmemoryram_${macaddress}.csv"
+touch "updatedmemoryram_${name}.csv"
 free -m -h | cat > memoryram.txt
 awk '{OFS=","};NR>1 {print $1,$2,$3,$4,$5}' memoryram.txt > memoryram.csv
-awk -v data="$dateinfo" -F"," 'BEGIN{OFS = ","}{$7=data; print}' memoryram.csv > "updatedmemoryram_${macaddress}.csv"
-cat "updatedmemoryram_${macaddress}.csv"
+awk -v data="$dateinfo" -F"," 'BEGIN{OFS = ","}{$7=data; print}' memoryram.csv > "updatedmemoryram_${name}.csv"
+cat "updatedmemoryram_${name}.csv"
