3689cad1d60535d9703042df5c313351a1391084
worstwebdev <94124964+worstwebdev@users.noreply.github.com>
2024-07-19 09:39:25 -0500

    Update2 allinfile.sh

--- allinfile.sh
+++ allinfile.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 macaddress=$(ip -o link show dev eno1 | grep -Po 'ether \K[^ ]*')
-touch allinfile_${macaddress}.txt
+ip=$(hostname -I | awk '{print $1}')
+name=$(cat /etc/hosts | grep $ip | awk '{print $2}')
+touch allinfile_${name}.txt
 
-totalcpu=$(top -bn2 | grep "Cpu(s)" | awk '{print $2+$4}' | cat | awk 'NR>1 {print $1}')
+totalcpu=$(top -bn1 -d0.01 | grep "Cpu(s)" | awk 'NR==1 {print $2 + $4}')
 totalram=$(free -h | awk '{print $3}' | tr -d 'fre, ' | sed 's/ //g' | awk 'NR<3 {print $1}')
 totaldisk=$(df -m | awk '{sum+=$5;} END{print sum;}')
-echo The cpu usage is $totalcpu, the total ram usage is $totalram, the total disk usage is $totaldisk. >> allinfile_${macaddress}.txt
-cat allinfile_${macaddress}.txt
+echo The cpu usage is $totalcpu, the total ram usage is $totalram, the total disk usage is $totaldisk. This is one machine $macaddress.> allinfile_${name}.txt
